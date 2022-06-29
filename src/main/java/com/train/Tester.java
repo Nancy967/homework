ef2b70339551bcf7409f3240c3abf538c5f855fb
Nancy967 <beg7520@gmail.com>
2022-06-29 19:53:51 +0800

    while(){}

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,13 +4,21 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.print("Please enter number of tickets: ");
+        System.out.println("Enter -1, if you have enough tickets.");
         Scanner scanner = new Scanner(System.in);
-        int totalTickets = scanner.nextInt();
-        System.out.print("How many round-trip tickets: ");
-        int roundTrip = scanner.nextInt();
+        int totalTickets = -2;
+        while (totalTickets != -1){
+            System.out.print("Please enter number of tickets: ");
+            totalTickets = scanner.nextInt();
+            if (totalTickets == -1){
+                break;
+            }
+            System.out.print("How many round-trip tickets: ");
+            int roundTrip = scanner.nextInt();
 
-        Tickets tickets=new Tickets(totalTickets, roundTrip);
-        tickets.print();
+            Tickets tickets=new Tickets(totalTickets, roundTrip);
+            tickets.print();
+        }
+        System.out.println("Thank you!");
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -3,20 +3,26 @@ package com.train
 import java.util.Scanner
 
 fun main() {
-    print("Please enter number of tickets: ")
-    val scanner = Scanner(System.`in`)
-    var totalTickets = scanner.nextInt()
-    print("How many round-trip tickets: ")
-    var roundTrip = scanner.nextInt()
+    println("Enter -1, if you have enough tickets.")
+    var totalTickets = -2
+    while (totalTickets != -1){
+        print("Please enter number of tickets: ")
+        totalTickets = readLine()!!.toInt()
+        if (totalTickets == -1){
+            break
+        }
+        print("How many round-trip tickets: ")
+        var roundTrip = readLine()!!.toInt()
 
-    val tickets = TesterKotlin(totalTickets, roundTrip)
-    tickets.print()
+        val tickets = TesterKotlin(totalTickets, roundTrip)
+        tickets.print()
+    }
+    println("Thank you!")
 }
 
 class TesterKotlin(var totalTickets:Int, var roundTrip:Int){
     fun print(){
-        print("Total tickets: " + totalTickets+"\n"
-                + "Round-trip: " + roundTrip+"\n"
-                + "Total: " +((totalTickets - roundTrip) * 1000 + roundTrip * 2000 * 0.9))
+        println("Total tickets:$totalTickets\nRound-trip:$roundTrip\nTotal:${total()}" )
     }
+    fun total() = (totalTickets - roundTrip) * 1000 + roundTrip * 2000 * 0.9
 }
